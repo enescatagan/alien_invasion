b394f3c0a82587efeba1910ff54b1efebc220e67
enescatagan <enescatagan@gmail.com>
2023-02-06 21:14:51 +0300

    Setting bacground color

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+
 class AlienInvation:
     """Overall class to manage game assets and behavior"""
 
@@ -12,15 +13,21 @@ class AlienInvation:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             # Event Loop
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():                
+            for event in pygame.event.get():
                 # When player clicks window's close button
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
