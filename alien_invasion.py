e6ad65c0abea81d6e3c3b31aede4615536ed2fab
enescatagan <enescatagan@gmail.com>
2023-02-07 17:00:56 +0300

    Refactoring: Update bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,13 +29,8 @@ class AlienInvation:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-
-            # Get rid of bullets that have dsappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
 
     def _check_events(self):
         """Respond to keypresses and mouse events"""
@@ -80,6 +75,16 @@ class AlienInvation:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet position.
+        self.bullets.update()
+
+        # Get rid of bullets that have dsappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
