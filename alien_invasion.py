40df0f7001d53b1eb2e5e74818dd1ecb0b044369
enescatagan <enescatagan@gmail.com>
2023-02-08 14:34:13 +0300

    Active Play Button for Begin

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,6 +57,11 @@ class AlienInvation:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            # Check Mouse button
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
             # Check KEYDOWN Events
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
@@ -64,6 +69,11 @@ class AlienInvation:
             # Check KEYUP Events
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
