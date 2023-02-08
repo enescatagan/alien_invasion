da6c89c654da85edbddfac3c0ebe7ead6bd9a151
enescatagan <enescatagan@gmail.com>
2023-02-08 16:10:53 +0300

    Reset Speed for new Game

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,6 +75,9 @@ class AlienInvation:
         # Flag for button click
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
